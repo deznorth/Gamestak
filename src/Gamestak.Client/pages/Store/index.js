e33d469f00ddb246da114230ecb31fe29a53df04
David Rojas <deznorth@gmail.com>
2020-11-29 02:26:35 -0500

    Added store page and set 1rem to 10px

---
+++ src/Gamestak.Client/pages/Store/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+import './style.scss';
+
+const Store = props => {
+  return (
+    <Container className="gs-store" fluid>
+      <h1>Hello, World!</h1>
+    </Container>
+  );
+};
+
+export default Store;
