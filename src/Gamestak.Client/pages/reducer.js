33d8bc1752b306ac372c668ae4c2220508058d9e
David Rojas <deznorth@gmail.com>
2020-12-09 02:32:12 -0500

    #20 - Library Page

--- src/Gamestak.Client/pages/reducer.js
+++ src/Gamestak.Client/pages/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import StoreReducer from './Store/modules/reducer';
+import LibraryReducer from './Library/modules/reducer';
 import GameDetailReducer from './GameDetail/modules/reducer';
 
 export default combineReducers({
   store: StoreReducer,
   detail: GameDetailReducer,
+  library: LibraryReducer,
 });
